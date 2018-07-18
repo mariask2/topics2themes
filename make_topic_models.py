6f8e15dcc618a20200fd67a368226b8a9010cf52
Maria Skeppstedt <mariask@dsv.su.se>
2018-07-18 15:50:45 +0200

    Sorted the texts on length before removing duplicates, to keep the shortest version

--- make_topic_models.py
+++ make_topic_models.py
@@ -207,23 +207,28 @@ def read_discussion_documents(data_label_list, cleaning_method, data_set_name):
     
     previous_sub_texts = set()
     
-    for file in file_list:
+    file_list_len_sorted = sorted(file_list, key=lambda x: len(x[TEXT]))
+    for file in file_list_len_sorted:
 
         # remove the first word, hashtag, re-tweet signal, if second word ends with colon,that word
         tokens_to_keep = []
   
         sp = file[TEXT].strip().split(" ")
+        
+        """
         for nr, word in enumerate(sp):
             if len(word) > 0:
                 if not word[0] == "#" and not word.lower() ==  "rt" and not "retw" in word.lower() and not (nr == 0 and word[-1] == ":"):
                     tokens_to_keep.append(word)
         text = " ".join(tokens_to_keep)
-
+        """
         n_gram_length = 6
+        if len(sp) < n_gram_length:
+            n_gram_length = len(sp)
         if True:
             add_this_file = True
             sub_tokens = []
-            for token in tokens_to_keep:
+            for token in sp:
                 sub_tokens.append(token)
                 if len(sub_tokens) > n_gram_length:
                     del sub_tokens[0]
