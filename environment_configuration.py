f8738200abab54ba1e7161f78a7d24988420707d
Maria Skeppstedt <mariask@dsv.su.se>
2019-02-15 18:25:43 +0100

    Added the current folder as the default environment configuration

--- environment_configuration.py
+++ environment_configuration.py
@@ -1,2 +1,5 @@
 
-WORKSPACE_FOLDER = "/Users/maria/mariaskeppstedtdsv/potsdam/visulizationideas/topic_modelling"
+WORKSPACE_FOLDER = "."
+#Here, give a path to the folder where you have the directory 'data_folder', which contains your data
+#For instance:
+#WORKSPACE_FOLDER = "/Users/maria/mariaskeppstedtdsv/potsdam/visulizationideas/topic_modelling"
