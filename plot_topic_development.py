85e6a19111008354a848b4c256cfa1b99a4f6b6d
Maria Skeppstedt <maria.skeppstedt@isof.se>
2021-01-13 15:05:34 +0100

    remove maring of first in year

--- plot_topic_development.py
+++ plot_topic_development.py
@@ -18,12 +18,12 @@ import matplotlib.pyplot as plt
 import json
 import matplotlib.markers as markers
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+from matplotlib import cm
 
 
 def plot_topics_text(topics_reps, texts_reps, topic_in_text_dict, title, file_name, min_year, max_year, year_title_tuple_list):
-    #marker_style = dict(color='tab:blue', linestyle=None, marker='o',
-    #                markersize=10, markerfacecoloralt='tab:red')
 
+    colors_map = ["blue", "orange", "green", "red", "purple", "brown", "deeppink", "olive", "darkturquoise"]
     fig, ax = plt.subplots()
     
     ax.set_xlim(min_year -1 , max_year + 1)
@@ -37,9 +37,11 @@ def plot_topics_text(topics_reps, texts_reps, topic_in_text_dict, title, file_na
     
     for y, topic_repr in enumerate(topics_reps):
         plt.axhline(y=y, linewidth=0.1, color='black')
+        
     
-    MOVE_X = 0.07
+    MOVE_X = 0.06
     last_iter_year = 0
+    color='black'
     for year, t in year_title_tuple_list:
         if last_iter_year != year:
             x_moved = 0.0
@@ -48,30 +50,33 @@ def plot_topics_text(topics_reps, texts_reps, topic_in_text_dict, title, file_na
        
         linewidth=0.02
         x = year + x_moved
-        color='black'
-        if x_moved == 0.0: # first line for year
-            extra = 0.01
-            linewidth = linewidth + extra*4
-            x = x - extra
+        if color=='black':
+            color='brown'
+        else:
+            color='black'
+        #if x_moved == 0.0: # first line for year
+        #    extra = 0.01
+        #    linewidth = linewidth + extra*4
+        #    x = x - extra
         plt.axvline(x=x, linewidth=linewidth, color=color)
         last_iter_year = year
             
+    #colors_map = cm.get_cmap('viridis', 8)
+    #
+  
+    #colors_map = cm.tab20b(np.linspace(0, 1, len(topics_reps))) #wasnt able to find a good color map
+
     
     for year, texts_for_year in topic_in_text_dict.items():
         for y, topic_repr in enumerate(topics_reps):
-            y_moved = 0.0
             x_moved = 0.0
-            for topic_in_text in texts_for_year:
-            
-            #ax.text(-0.5, y, repr(topic_repr),
-             #   horizontalalignment='center', verticalalignment='center')
+            for topic_in_text in texts_for_year: # go through scatter-vector for each text for the year
                 marker = markers.MarkerStyle(marker='|', fillstyle='none')
-                #ax.scatter(year+ x_moved, y + y_moved, s = topic_in_text[y]*10, edgecolors = "face", c="grey", alpha = 0.7, linewidths = 0.05, marker='o')
-                if topic_in_text[y] != 0:
-                    ax.plot([year + x_moved, year + x_moved], [y + topic_in_text[y]/10, y - topic_in_text[y]/10], 'r+-', linewidth=0.4, markersize=topic_in_text[y]/5)
-                #edgecolors="black"
-                y_moved = y_moved - 0.07
+                if topic_in_text[y] != 0: #if the text contains the topic
+                    ax.plot([year + x_moved, year + x_moved], [y + topic_in_text[y]/10, y - topic_in_text[y]/10], '+-', linewidth=0.2, markersize=topic_in_text[y]/5, color = "red")
                 x_moved = x_moved + MOVE_X
+
+                    
     plt.xticks(fontsize=5, rotation=90)
     plt.yticks(fontsize=5)
     #for tick in ax.get_xticklabels():
@@ -136,9 +141,9 @@ for el in obj["topic_model_output"]["documents"]:
         topic_info["terms_found_in_text"] = t["terms_found_in_text"]
         topic_info["topic_index"] = t["topic_index"]
         topic_info["topic_confidence"] = t["topic_confidence"]
-        document_topics.append(topic_info)
         if len(topic_info["terms_found_in_text"]) > 1: # At least two terms included in text to include
-            document_info[base_name] = {"document_topics" : document_topics, "label": label}
+            document_topics.append(topic_info)
+    document_info[base_name] = {"document_topics" : document_topics, "label": label}
     #print(json.dumps(el, indent = 1))
     #print(base_name, label)
 
@@ -172,6 +177,9 @@ for el in obj["topic_model_output"]["topics"]:
 scatter_dict = {}
 year_title_tuple_list = []
 x_labels = []
+nr_documents_to_scatter_dict = 0
+nr_documents_to_scatter_dict_with_topics_found = 0
+nr_documents_not_associated_with_topic_in_model = 0
 for el in editorial_data_list_science:
     id = el[0]
     topic_found = False
@@ -185,17 +193,21 @@ for el in editorial_data_list_science:
     scatter_for_editorial = [0]*len(topics.keys())
     if id in document_info: # topic in document
         #print(document_info[el[0]])
-        for topic_in_document in document_info[el[0]]["document_topics"]:
+        for topic_in_document in document_info[id]["document_topics"]:
            index_for_topic_in_scatter = sorted(topics.keys()).index(topic_in_document["topic_index"])
            scatter_for_editorial[index_for_topic_in_scatter] = topic_in_document["topic_confidence"]
-        pass
+           topic_found = True
     else:
-        #print(el[0], "missing")
-        pass
+        nr_documents_not_associated_with_topic_in_model = nr_documents_not_associated_with_topic_in_model + 1
+        
     scatter_dict[year].append(scatter_for_editorial)
+    nr_documents_to_scatter_dict = nr_documents_to_scatter_dict + 1
+    if topic_found:
+        nr_documents_to_scatter_dict_with_topics_found = nr_documents_to_scatter_dict_with_topics_found + 1
 
-
-#print(x_labels)
+print("nr_documents_to_scatter_dict", nr_documents_to_scatter_dict)
+print("nr_documents_to_scatter_dict_with_topics_found", nr_documents_to_scatter_dict_with_topics_found)
+print("nr_documents_not_associated_with_topic_in_model", nr_documents_not_associated_with_topic_in_model)
 
 #plot_topics_text(["tax, forrest, tree", "bush, obama, administration, word4, word5", "oceans, pollution", "media, science"], ["(Article nr 1) 1988", "(Article nr 1) 1989", "(Article nr 1) 1990"], [[2, 0, 5, 7], [0, 3, 4, 5], [2, 4, 0, 7]], "test2", "test2")
 
