9a18237a74d4ad9f3df2504303150a807eaff86d
marsk757 <maria.skeppstedt@abm.uu.se>
2023-01-15 12:23:20 +0100

    Read pickled test data

--- save_test_data.py
+++ save_test_data.py
@@ -17,7 +17,12 @@ def get_data():
                 text_list.append(s.strip())
                 
     print("Nr of sentences: ", len(text_list))
-    text_dict = {TEXT: text_list}
+    larger_list = []
+    for i in range(0, LARGE_SET_SIZE):
+        larger_list.extend(text_list)
+    print("Nr of sentences in larger list: ", len(larger_list))
+        
+    text_dict = {TEXT: larger_list}
     df = pd.DataFrame(text_dict)
     print("Original dataframe: ")
     print(df)
@@ -33,17 +38,30 @@ def get_data():
     print("Now as: ", type(read_list))
     print("Nr of sentences: ", len(read_list))
     
-    larger_list = []
-    for i in range(0, LARGE_SET_SIZE):
-        larger_list.extend(read_list)
-    print("Nr of sentences in larger list: ", len(larger_list))
+    
+    
     nr_of_new_york = 0
-    for el in larger_list:
+    for el in read_list:
         if "New York" in el:
             nr_of_new_york = nr_of_new_york + 1
     print("Nr of times New York is mentioned:", nr_of_new_york)
+
+def read_documents(data_label_list, data_set_name, cleaning_method, n_gram_length_conf, remove_duplicates):
+    meta_data_list = None # No metadata
+    
+    read_df = pd.read_pickle(file_name)
+    print("Read dataframe: ")
+    print(read_df)
+    
+    print("Back to list: ")
+    read_list = read_df["text"].tolist()
+    print("Now as: ", type(read_list))
+    print("Nr of sentences: ", len(read_list))
+    
+    return read_list, meta_data_list
     
 get_data()
-
+read_list, meta_data_list = read_documents(None, None, None, None, False)
+print("Nr of sentences: ", len(read_list))
 # Creating from dict of list
 
